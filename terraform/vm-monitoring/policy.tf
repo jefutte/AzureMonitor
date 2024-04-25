6bc208f9dd20ca777c25fb4c6ed80dc225607566
jefutte <ext_jbift@arlafoods.com>
2024-04-25 14:56:26 +0200

    different parameter approach

--- terraform/vm-monitoring/policy.tf
+++ terraform/vm-monitoring/policy.tf
@@ -1,27 +1,11 @@
 locals {
-  parameters = jsonencode({
-    "enableProcessesAndDependencies" = {
-      value = false
-    }
-    "bringYourOwnUserAssignedManagedIdentity" = {
-      value = true
-    }
-    "userAssignedManagedIdentityName" : {
-      value = "${azurerm_user_assigned_identity.name}"
-    }
-    "userAssignedManagedIdentityResourceGroup" : {
-      value = "${azurerm_resource_group.name}"
-    }
-    "effect" : {
-      value = "DeployIfNotExists"
-    }
-    "scopeToSupportedImages" : {
-      value = true
-    }
-    "dcrResourceId" : {
-      value = "${azurerm_monitor_data_collection_rule.id}"
-    }
-  })
+    enableProcessesAndDependencies = false
+    bringYourOwnUserAssignedManagedIdentity = true
+    userAssignedManagedIdentityName = azurerm_user_assigned_identity.uami.name
+    userAssignedManagedIdentityResourceGroup = azurerm_resource_group.monitorrg.name 
+    effect = "DeployIfNotExists"
+    scopeToSupportedImages = true
+    dcrResourceId = azurerm_monitor_data_collection_rule.vmidcr.id
 }
 
 resource "azurerm_subscription_policy_assignment" "subpol" {
@@ -29,5 +13,29 @@ resource "azurerm_subscription_policy_assignment" "subpol" {
   subscription_id      = data.azurerm_client_config.core.subscription_id
   policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/924bfe3a-762f-40e7-86dd-5c8b95eb09e6"
 
-  parameters = local.parameters
+  parameters = jsonencode(
+    {
+        "enableProcessesAndDependencies" = {
+            value = local.enableProcessesAndDependencies
+        }
+        "bringYourOwnUserAssignedManagedIdentity" = {
+            value = local.bringYourOwnUserAssignedManagedIdentity
+        }
+        "userAssignedManagedIdentityName" : {
+            value = local.userAssignedManagedIdentityName
+        }
+        "userAssignedManagedIdentityResourceGroup" : {
+            value = local.userAssignedManagedIdentityResourceGroup
+        }
+        "effect" : {
+            value = local.effect
+        }
+        "scopeToSupportedImages" : {
+            value = local.scopeToSupportedImages
+        }
+        "dcrResourceId" : {
+            value = local.dcrResourceId
+        }
+    }
+  )
 }
