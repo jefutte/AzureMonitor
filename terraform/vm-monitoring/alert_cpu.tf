7bd9ae869d34df2dab33375d65d3b8d70caf6888
jefutte <ext_jbift@arlafoods.com>
2024-06-05 13:58:30 +0200

    added action group to alerts

--- terraform/vm-monitoring/alert_cpu.tf
+++ terraform/vm-monitoring/alert_cpu.tf
@@ -53,4 +53,10 @@ resource "azurerm_monitor_scheduled_query_rules_alert_v2" "alert_cpu" {
   auto_mitigation_enabled          = true
   workspace_alerts_storage_enabled = false
 
+  action {
+    action_groups = [ 
+      azurerm_monitor_action_group.actions.id
+    ]
+  }
+
 }
