490587757e1150e0ace06d1d51c1a62c84c5299a
jefutte <ext_jbift@arlafoods.com>
2024-06-13 17:18:55 +0200

    role scope fix

--- terraform/vm-monitoring/policy.tf
+++ terraform/vm-monitoring/policy.tf
@@ -61,7 +61,7 @@ resource "azurerm_role_assignment" "role_az" {
   for_each = local.azRoles
 
   role_definition_name = each.key
-  scope = data.azurerm_client_config.core.subscription_id
+  scope = "/subscriptions/${data.azurerm_client_config.core.subscription_id}"
   principal_id = azurerm_subscription_policy_assignment.subpol.identity[0].principal_id
 }
 
@@ -95,6 +95,6 @@ resource "azurerm_role_assignment" "role_hybrid" {
   for_each = local.hybridRoles
 
   role_definition_name = each.key
-  scope = data.azurerm_client_config.core.subscription_id
+  scope = "/subscriptions/${data.azurerm_client_config.core.subscription_id}"
   principal_id = azurerm_subscription_policy_assignment.subpol_hybrid.identity[0].principal_id
 }
